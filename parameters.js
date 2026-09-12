c3cf523fa0991926778d1a33ad7c798f09f45dc8
KaishuIinuma <148365118+KaishuIinuma@users.noreply.github.com>
2026-09-13 08:14:16 +0900

    倍率の変更

--- parameters.js
+++ parameters.js
@@ -96,7 +96,7 @@ const CONNECTION_DASH_SPEED = 40;  // 接続線のアニメーション速度（
 const IS_DEV_MODE = true;
 
 // 開発モード中に、選択図形をマウスホイールで拡大・縮小する倍率
-const DEBUG_WHEEL_SCALE_STEP = 0.08;
+const DEBUG_WHEEL_SCALE_STEP = 0.02;
 
 // デバッグ画像の倍率（1.0 = キャンバスに収める、0.5 = 半分、2.0 = 2倍）
 const DEBUG_IMAGE_SCALE = 0.7;
